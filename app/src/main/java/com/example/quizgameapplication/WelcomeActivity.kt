99737de772e41c58ce42ca697f6b1de0fdebfb7e
janaka120 <janakastill@gmail.com>
2023-08-01 23:33:05 +0800

    Create project structure and add splash screen

---
+++ app/src/main/java/com/example/quizgameapplication/WelcomeActivity.kt
@@ -0,0 +1,34 @@
+package com.example.quizgameapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import com.example.quizgameapplication.databinding.ActivityWelcomeBinding
+
+class WelcomeActivity : AppCompatActivity() {
+
+    lateinit var activityWelcomeBinding: ActivityWelcomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityWelcomeBinding = ActivityWelcomeBinding.inflate(layoutInflater)
+        var view = activityWelcomeBinding.root
+        setContentView(view)
+
+        val alphaAnimation = AnimationUtils.loadAnimation(applicationContext, R.anim.splash_anim)
+        activityWelcomeBinding.textView.startAnimation(alphaAnimation)
+
+        val handler = Handler(Looper.getMainLooper())
+
+        handler.postDelayed(object: Runnable {
+            override fun run() {
+                val intent = Intent(this@WelcomeActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 50000)
+    }
+}
