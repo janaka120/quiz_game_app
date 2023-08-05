7311c8513456ffe86f2afba89256363492c0d531
janaka120 <janakastill@gmail.com>
2023-08-05 13:33:57 +0800

    add login with email and password flow

--- app/src/main/java/com/example/quizgameapplication/LoginActivity.kt
+++ app/src/main/java/com/example/quizgameapplication/LoginActivity.kt
@@ -3,10 +3,15 @@ package com.example.quizgameapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.quizgameapplication.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     lateinit var loginBinding: ActivityLoginBinding
+
+    val authFirebase = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +21,12 @@ class LoginActivity : AppCompatActivity() {
         setContentView(view)
 
         loginBinding.buttonLoginSignIn.setOnClickListener {
-
+            val email = loginBinding.editTextLoginEmail.text.toString()
+            val password = loginBinding.editTextLoginPassword.text.toString()
+            loginWithEmailAndPassword(email, password)
         }
 
         loginBinding.buttonLoginGoogleSignIn.setOnClickListener {
-
         }
 
         loginBinding.textViewLoginSignUp.setOnClickListener {
@@ -30,6 +36,36 @@ class LoginActivity : AppCompatActivity() {
 
         loginBinding.textViewLoginForgotPassword.setOnClickListener {
 
+        }
+
+    }
+        fun loginWithEmailAndPassword(email: String, password: String) {
+            loginBinding.progressBarLogin.visibility = View.VISIBLE
+            loginBinding.buttonLoginSignIn.isClickable = false
+            authFirebase.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    Toast.makeText(applicationContext, "Welcome to Quiz app", Toast.LENGTH_SHORT).show()
+                    finish()
+                }else {
+                    Toast.makeText(applicationContext, task.exception?.localizedMessage, Toast.LENGTH_SHORT).show()
+                    println("User login error >>>" + task.exception?.localizedMessage)
+                }
+
+                loginBinding.progressBarLogin.visibility = View.INVISIBLE
+                loginBinding.buttonLoginSignIn.isClickable = true
+            }
+        }
+
+    override fun onStart() {
+        super.onStart()
+
+        val user = authFirebase.currentUser
+        if (user != null) {
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(applicationContext, "Welcome to Quiz app", Toast.LENGTH_SHORT).show()
         }
     }
 }
