9f6aee9fb62706b99c2baffc4bf48292d806bab5
janaka120 <janakastill@gmail.com>
2023-08-05 13:34:23 +0800

    add logout functionality

--- app/src/main/java/com/example/quizgameapplication/MainActivity.kt
+++ app/src/main/java/com/example/quizgameapplication/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.quizgameapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quizgameapplication.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+        setContentView(view)
+
+        mainBinding.buttonMainSignOut.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
